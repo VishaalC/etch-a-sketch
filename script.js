0a380508232b97aa9508feaf4fb75f68f4242609
VishaalC <vishaalc1@gmail.com>
2023-01-19 00:36:52 +0530

    Finish Application

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 let container = document.querySelector('.container');
+let size = prompt("Enter grid size: ");
 
-for (let index = 0; index < 16; index++) {
-    for (let index = 0; index < 16; index++) {
+var root = document.querySelector(':root');
+root.style.setProperty('--size', size);
+
+function generateRandomColor(){
+    let maxVal = 0xFFFFFF; // 16777215
+    let randomNumber = Math.random() * maxVal; 
+    randomNumber = Math.floor(randomNumber);
+    randomNumber = randomNumber.toString(16);
+    let randColor = randomNumber.padStart(6, 0);   
+    return `#${randColor.toUpperCase()}`
+}
+console.log(generateRandomColor()); 
+
+for (let index = 0; index < size; index++) {
+    for (let i = 0; i < size ;i++) {
         let div = document.createElement('div');
         div.textContent = "\u00a0";
         div.classList.add('square');
@@ -10,10 +24,26 @@ for (let index = 0; index < 16; index++) {
 }
 
 let squares = document.querySelectorAll('.square');
-for (const square of squares) {
-    square.addEventListener('mouseover', () => {
-        console.log("hovering");
-        square.classList.add("color");
-    })
+
+function black() {
+    for (const square of squares) {
+        square.addEventListener('mouseover', () => {
+            console.log("hovering");
+            square.classList.add("color");
+        })
     
+    }
+}
+
+function rainbow() {
+    for (const square of squares) {
+        square.addEventListener('mouseover', (e) => {
+            console.log("hovering");
+            e.target.style.backgroundColor = generateRandomColor();
+        })
+    }
+}
+
+function reset() {
+    location.reload();
 }
